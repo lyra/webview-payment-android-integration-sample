4060d89f34ff84961b005d4ba5dd823f40eac925
Soler Alexandre <alexandre.soler@lyra-network.com>
2022-06-15 17:54:26 +0200

    Update example to 1.2 version

--- app/src/main/java/com/lyranetwork/sampleandroidwebview/payment/AbstractPaymentActivity.kt
+++ app/src/main/java/com/lyranetwork/sampleandroidwebview/payment/AbstractPaymentActivity.kt
@@ -1,7 +1,7 @@
 package com.lyranetwork.sampleandroidwebview.payment
 
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import org.json.JSONObject
 
 /**
